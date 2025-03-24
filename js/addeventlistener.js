1d9a8d4f2d825c2b74664f422f891f9dd95af762
whales-code <106753020@qq.com>
2025-03-24 20:19:29 +0800

    Site updated: 2025-03-24 20:19:28

--- js/addeventlistener.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // DOM 加载完成后执行的代码
-    const button = document.getElementById('myButton');
-    if (button) {
-        button.addEventListener('click', () => {
-            console.log('按钮被点击了！');
-        });
-    }
-});
